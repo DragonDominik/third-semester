05678c34014ea78963a6851494ee3774728d0d53
Dragon Dominik <dragondominik0103@gmail.com>
2025-11-10 17:29:45 +0100

    eva 9ora

---
+++ EVA/9ora/elte_eva_gy09_skeleton/PingPongGame.Skeleton/PingPongGame/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using PingPongGame.ViewModel;
+using ELTE.PingPongGame.Model;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PingPongGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private MainWindow _window;
+        private MainViewModel _mainViewModel;
+
+        public App()
+        {
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            _mainViewModel = new MainViewModel();
+            _mainViewModel.PadNextPosition += OnPadNextPosition;
+            _mainViewModel.BallNextPosition += OnBallNextPosition;
+            _mainViewModel.GameOver += OnGameOver;
+
+
+            _window = new MainWindow()
+            {
+                DataContext = _mainViewModel
+            };
+
+            _window.BallLayoutUpdated += OnBallLayoutUpdated;
+            _window.PadLayoutUpdated += OnPadLayoutUpdated;
+
+            _window.Show();
+        }
+
+        private void OnGameOver(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Game over");
+            if ( result == MessageBoxResult.OK)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void OnBallNextPosition(object sender, Thickness e)
+        {
+            _window.StartBallAnimation(e);
+        }
+
+        private void OnPadNextPosition(object sender, Thickness e)
+        {
+            _window.StartPadAnimation(e);
+        }
+
+        private void OnBallLayoutUpdated(object sender, Thickness e)
+        {
+            _mainViewModel.MoveBall(e.Left, e.Top);
+        }
+
+        private void OnPadLayoutUpdated(object sender, Thickness e)
+        {
+            _mainViewModel.MovePad(e.Left, e.Top);
+        }
+    }
+
+}
---
+++ EVA/9ora/elte_eva_gy09_skeleton/PingPongGame.Skeleton/PingPongGame/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using PingPongGame.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PingPongGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public EventHandler<Thickness>? BallLayoutUpdated;
+        public EventHandler<Thickness>? PadLayoutUpdated;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartBallAnimation(Thickness nextPosition)
+        {
+            ThicknessAnimation animation = new ThicknessAnimation
+            {
+                From = Ball.Margin,
+                To = nextPosition,
+                Duration = new Duration(TimeSpan.FromMilliseconds(5)),
+                SpeedRatio = 1 / TravelDistance(Ball.Margin, nextPosition)
+                // Speed depends on the distance to travel
+            };
+            Ball.BeginAnimation(Ellipse.MarginProperty, animation,
+            HandoffBehavior.SnapshotAndReplace);
+        }
+
+        public void StartPadAnimation(Thickness nextPosition)
+        {
+            ThicknessAnimation animation = new ThicknessAnimation
+            {
+                From = Pad.Margin,
+                To = nextPosition,
+                Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+            };
+            Pad.BeginAnimation(Rectangle.MarginProperty, animation,
+            HandoffBehavior.SnapshotAndReplace);
+        }
+
+        private void OnBallLayoutUpdated(object? sender, EventArgs e)
+        {
+            BallLayoutUpdated?.Invoke(this, Ball.Margin);
+        }
+
+        private void OnPadLayoutUpdated(object? sender, EventArgs e)
+        {
+            PadLayoutUpdated?.Invoke(this, Pad.Margin);
+        }
+
+        private double TravelDistance(Thickness currentPosition, Thickness nextPosition)
+        {
+            double dx = nextPosition.Left - currentPosition.Left;
+            double dy = nextPosition.Top - currentPosition.Top;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
